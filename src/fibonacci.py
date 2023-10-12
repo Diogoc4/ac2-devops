abafc91af4e9eeebf35b8de0dad545c5bd9efb5b
Diogoc4 <diogo.cruz@aluno.faculdadeimpacta.com.br>
2023-10-12 10:31:25 -0300

    travis add

---
+++ src/fibonacci.py
@@ -0,0 +1,13 @@
+n1 = 0
+n2 = 1
+
+print("SEQUENCIA DE FIBONACCI")
+for _ in range(12):
+    if (_<2):
+        print(_)
+    else:
+        n3 = n1+n2
+        print(f"{n3}")
+        n1 = n2
+        n2 = n3        
+
