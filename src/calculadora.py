1119436086b4b305d8286924f15813c9c4b2a3cb
julia_ribeiro <113191131+juapparecido@users.noreply.github.com>
2023-10-12 19:15:09 -0300

    Create calculadora.py

---
+++ src/calculadora.py
@@ -0,0 +1,31 @@
+def calculadora():
+    print("Selecione a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
+    escolha = input("Digite sua escolha(1/2/3/4): ")
+
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+
+    if escolha == '1':
+        print(num1, "+", num2, "=", num1 + num2)
+
+    elif escolha == '2':
+        print(num1, "-", num2, "=", num1 - num2)
+
+    elif escolha == '3':
+        print(num1, "*", num2, "=", num1 * num2)
+
+    elif escolha == '4':
+        if num2 != 0:
+            print(num1, "/", num2, "=", num1 / num2)
+        else:
+            print("Erro! Divisão por zero não é permitida.")
+
+    else:
+        print("Entrada Inválida")
+
+calculadora()
