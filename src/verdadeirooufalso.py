cb05d5ff2e8f9f2f1ea4d77ec800442420e347f9
julia_ribeiro <113191131+juapparecido@users.noreply.github.com>
2023-10-14 12:35:26 -0300

    Delete src/verdadeirooufalso.py

--- src/verdadeirooufalso.py
+++
@@ -1,6 +0,0 @@
-def avaliar_expressao():
-    expressao = input("Digite uma expressão booleana (por exemplo, 5 > 3): ")
-    resultado = eval(expressao)
-    print("O resultado da expressão é:", resultado)
-
-avaliar_expressao()
